68fa7e2d01765aa583112720da70f9c6f373a671
FabioRieker <faabiioo05@gmail.com>
2026-09-25 10:16:52 +0200

    añadir página de usuarios y servicio para listar usuarios activos

---
+++ usersApp/src/app/pages/users/users.page.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonSpinner,
+  IonList,
+  IonItem,
+  IonLabel
+} from '@ionic/angular';
+import { UsersService } from '../../services/users.service';
+import { User } from '../../models/user.model';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.page.html',
+  styleUrls: ['./users.page.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonSpinner,
+    IonList,
+    IonItem,
+    IonLabel
+  ]
+})
+export class UsersPage implements OnInit {
+  users: User[] = [];
+  loading = false;
+
+  constructor(
+    private usersService: UsersService,
+    private cdr: ChangeDetectorRef
+  ) { }
+
+  async ngOnInit() {
+    await this.loadUsers();
+  }
+
+  async loadUsers() {
+    try {
+      this.loading = true;
+      this.cdr.detectChanges();
+      this.users = await this.usersService.getActiveUsers();
+      console.log('Usuarios cargados correctamente:', this.users);
+    } catch (error) {
+      console.error('Error al obtener usuarios:', error);
+    } finally {
+      this.loading = false;
+      this.cdr.detectChanges();
+    }
+  }
+}
